64fa4500799bb99cae89cc1ee797c735d9044762
Ntthao83053 <thao83053@gmail.com>
2025-06-25 07:51:30 +0000

    Thao cap nhat

---
+++ nhahang/src/khachhang.java
@@ -0,0 +1,6 @@
+public class khachhang {
+    String hovaten ;
+    String tuoi ;
+    String sodienthoai ;
+    public khachhang(String name ; String tuoi; String sdt)
+}
---
+++ nhahang/test/testnhanvien.java
@@ -0,0 +1,6 @@
+public class testnhanvien {
+    public void test1(){
+        nhanvien nv = new nhanvien(name:"Nguyen Van A", tuoi:"21", sdt:"098765358", dc:"bac", gioitinh:"nu", cccd:"7543546");
+        nv.hienthi1();
+    }
+}
