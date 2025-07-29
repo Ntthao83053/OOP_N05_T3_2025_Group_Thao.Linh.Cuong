44bcff15d1d76b26ab3eba081b8567ac65ab3697
Ntthao83053 <thao83053@gmail.com>
2025-07-29 10:45:48 +0000

    cap nhat

--- nhahang/src/App.java
+++ nhahang/src/App.java
@@ -1,5 +1,13 @@
-
+import java.util.ArrayList;
+// Add the following import if TestQuanLyDatBan is in another package
+// import your.package.name.TestQuanLyDatBan;
 
 public class App {
-
+    public static void main(String[] args) {
+        DatBanController quanLy = new DatBanController();
+        ArrayList<DatBan> danhSachDatBan = quanLy.nhapDanhSachDatBan();
+        TestDatBanController.kiemThuHienThiDatBanTheoNgay(danhSachDatBan);
+    }
 }
+
+
---
+++ nhahang/src/moder/DatBan.java
@@ -0,0 +1,16 @@
+public class DatBan {
+    String tenKhachHang;
+    int soNguoi;
+    String gioDat;
+    int soBan;
+    String ngayDat;
+
+    
+    public DatBan(String tenKhachHang, int soNguoi, String gioDat, int soBan, String ngayDat) {
+        this.tenKhachHang = tenKhachHang;
+        this.soNguoi = soNguoi;
+        this.gioDat = gioDat;
+        this.soBan = soBan;
+        this.ngayDat = ngayDat;
+    }
+}
